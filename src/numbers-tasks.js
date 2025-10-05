a11a9766d33fa71795be988f37dd68ee38d2b658
bearBenjamin <git@github.com:bearBenjamin/cinemaddict.git>
2025-10-05 22:29:24 +0300

    feat: add 7 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -108,8 +108,13 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const scalarProduct = x1 * x2 + y1 * y2;
+  const distanceOne = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const distanceTwo = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const cos = scalarProduct / (distanceOne * distanceTwo);
+  const result = Math.acos(cos);
+  return result;
 }
 
 /**
@@ -125,8 +130,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -140,8 +145,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -157,8 +162,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -178,8 +183,11 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const tenPower = 10 ** pow;
+  const newNum = Math.round(num / tenPower);
+  const result = newNum * tenPower;
+  return result;
 }
 
 /**
@@ -199,8 +207,19 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 1) return n;
+
+  if (n > 1) {
+    const sqrN = Math.sqrt(n);
+    for (let i = 2; i <= sqrN; i += 1) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
 }
 
 /**
